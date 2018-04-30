2869abb01d3e1cdfe053f120d410accf49f7648a
Daksha Patel <dakshapatel100@gmail.com>
2018-04-30 00:06:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,7 @@ def input_to_index(user_input)
     s - 1
  end
 
-def move(location, player = "X")
+def move(location, player = "X" || "O")
   @board[location] = player
   turn_count
   display_board
